a8eb2b108440d58999ff3c088e6998046efb0938
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2022-10-06 15:08:24 -0400

    Use new camera to render blue white gradient

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
+#include "./lib/Camera.h"
 #include "./lib/Image.h"
-#include "./lib/Vec3.h"
+
+#include <iostream>
 
 int main() {
   Image image("test.png", 256, 256, 3);
 
+  const double viewportHeight = 2.0;
+  Camera<> camera(viewportHeight * image.aspectRatio, viewportHeight, 1.0,
+                  Point3<>(0, 0, 0));
+
   for (int j = 0; j < image.height; j++) {
     for (int i = 0; i < image.width; i++) {
-      Color3 color(double(i) / (image.width - 1),
-                   double(j) / (image.height - 1), 0.25);
-
+      double x = double(i) / (image.width - 1);
+      double y = double(j) / (image.height - 1);
+      auto color = camera.getColor(x, y);
       image.pushPixel(color * 255);
     }
   }
